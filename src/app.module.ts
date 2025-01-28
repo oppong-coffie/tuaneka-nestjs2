59af5e349f9c2563654c2bee0f4b13f49cc2171d
oppong-coffie <teama@teamalfy.com>
2025-01-28 18:22:01 +0000

    envadded

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { MessagingController } from './messaging/messaging.controller';
 import { MessagingService } from './messaging/messaging.service';
 import { MessagingModule } from './messaging/messaging.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MessagingModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    //connect to mongodb
+    MongooseModule.forRoot(process.env.MONGO_UI),
+    MessagingModule
+  ],
   controllers: [AppController, MessagingController],
   providers: [AppService, MessagingService],
 })
